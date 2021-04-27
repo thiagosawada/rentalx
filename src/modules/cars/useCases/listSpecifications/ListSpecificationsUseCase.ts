af17b7ed850c58621deb188a920981833eff9a79
Thiago <tms1_tms2@hotmail.com>
2021-04-27 11:04:35 -0300

    First commit

---
+++ src/modules/cars/useCases/listSpecifications/ListSpecificationsUseCase.ts
@@ -0,0 +1,13 @@
+import { Specification } from "../../models/Specification";
+import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository";
+
+class ListSpecificationsUseCase {
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute(): Specification[] {
+    const specifications = this.specificationsRepository.list();
+    return specifications;
+  }
+}
+
+export { ListSpecificationsUseCase };
