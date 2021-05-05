f7f8746de226f2887f148b932e095d902388a320
Thiago <tms1_tms2@hotmail.com>
2021-05-05 15:48:05 -0300

    Create DayhsDateProvider

---
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -0,0 +1,28 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+
+import { IDateProvider } from "../IDateProvider";
+
+dayjs.extend(utc);
+
+class DayjsDateProvider implements IDateProvider {
+  dateNow(): Date {
+    return dayjs().toDate();
+  }
+
+  compareInHours(start_date: Date, end_date: Date): number {
+    const endDateUTC = this.convertToUTC(end_date);
+    const startDateUTC = this.convertToUTC(start_date);
+    return dayjs(endDateUTC).diff(startDateUTC, "hours");
+  }
+
+  convertToUTC(date: Date): string {
+    return dayjs(date).utc().local().format();
+  }
+
+  tomorrow(): Date {
+    return dayjs().add(1, "day").toDate();
+  }
+}
+
+export { DayjsDateProvider };
