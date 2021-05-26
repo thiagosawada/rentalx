6b51bacd8900dc65b015162bec59a76d5199d850
Thiago <tms1_tms2@hotmail.com>
2021-05-26 16:32:03 -0300

    Upload files to aws s3

--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from "tsyringe";
 
 // import { CarImage } from "@modules/cars/infra/typeorm/entities/CarImage";
 import { ICarImagesRepository } from "@modules/cars/repositories/ICarImagesRepository";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 
 interface IRequest {
   car_id: string;
@@ -12,12 +13,15 @@ interface IRequest {
 class UploadCarImagesUseCase {
   constructor(
     @inject("CarImagesRepository")
-    private carImagesRepository: ICarImagesRepository
+    private carImagesRepository: ICarImagesRepository,
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
 
   async execute({ car_id, images_name }: IRequest): Promise<void> {
     images_name.map(async (image) => {
       await this.carImagesRepository.create(car_id, image);
+      await this.storageProvider.save(image, "cars");
     });
   }
 }
